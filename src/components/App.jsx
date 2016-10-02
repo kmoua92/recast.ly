c914e1996a5f71c2f6b43a3a596dbac739107648
kmoua92 <kermoua92@gmail.com>
2016-10-01 17:17:18 -0700

    Implement automatic rendering when video title clicked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,31 @@
-var App = (props) => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={props.videos} />
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: this.props.videos,
+      currentVideo: this.props.videos[0]
+    };
+  }
+
+  handleVideoListEntryClick (video) {
+    this.setState({currentVideo: video});
+  }
+
+  
+  render(props) {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videos} handleVideoListEntryClick={this.handleVideoListEntryClick.bind(this)} />
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,6 +1,6 @@
 var VideoList = (props) => (
   <div className="video-list media"> 
-    {props.videos.map(video => <VideoListEntry video={video} key={video.id.videoId}/>)}  
+    {props.videos.map(video => <VideoListEntry video={video} key={video.id.videoId} handleVideoListEntryClick={props.handleVideoListEntryClick}/>)}  
   </div>
 );
 
